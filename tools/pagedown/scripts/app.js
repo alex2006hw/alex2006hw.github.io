a1729848ff9c53e1e9a8b751002c2f059f187204
Alex Davis <alex@Alexs-MacBook-Pro.local>
2014-11-04 13:29:14 -0800

    Build Tue Nov  4 13:29:11 PST 2014

---
+++ tools/pagedown/scripts/app.js
@@ -0,0 +1,16 @@
+﻿angular.module('MarkdownModule', [])
+    .directive('pagedown', function () {
+        return {
+            restrict: 'E',
+            template:
+                '<div id="wmd-button-bar"></div>' +
+                '<textarea id="wmd-input"></textarea>' +
+                '<div id="wmd-preview"></div>',
+            link: function () {
+                Object.prototype.tooltip = function () { }
+                var converter = Markdown.getSanitizingConverter();
+                var editor = new Markdown.Editor(converter);
+                editor.run();
+            }
+        };
+    });
